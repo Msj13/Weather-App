0bed4b960444265e531315e27ba307fce562cff9
Msj13 <105527722+Msj13@users.noreply.github.com>
2022-07-31 16:31:49 -0500

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f043f6acfbmsh7744f4669e09dcap121cd5jsn486256659b5c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "1dbd534512583fd24b118655b40ad79c"
